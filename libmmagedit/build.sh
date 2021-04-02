cdc3b38ad54b9da38418ebb00230dc4281028e10
nstbayless <nstbayless@gmail.com>
2021-04-02 13:48:28 -0700

    updated build linux libmmagedit build script

--- libmmagedit/build.sh
+++ libmmagedit/build.sh
@@ -3,13 +3,13 @@ CPPARGS="-fPIC -static-libstdc++ -I/usr/include/python3.9/ -lpython3.9"
 set -e
 echo "building..."
 
-g++ -shared -o libmmagedit.so  mmagedit.cpp  $CPPARGS
-g++ -o mmagedit  mmagedit.cpp  $CPPARGS -DMAIN -g -DLOCAL_PYTHON_H
+g++ -static-libstdc++ -shared -o libmmagedit.so  mmagedit.cpp  $CPPARGS
+g++ -static-libstdc++ -o mmagedit  mmagedit.cpp  $CPPARGS -DMAIN -g -DLOCAL_PYTHON_H
 
 if command -v patchelf > /dev/null
 then
     patchelf --set-rpath "\$ORIGIN" libmmagedit.so
-    patchelf --set-rpath "\$ORIGIN" mmagedit
+    patchelf --set-rpath "\$ORIGIN:\$ORIGIN/lib" mmagedit
 else
     echo "please install patchelf to add \$ORIGIN to libmmagedit RPATH"
 fi
